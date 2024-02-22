09842c52e682b4b33ebd37724529bd42028c517a
Mathiew82 <amateo1982@yahoo.es>
2024-02-22 21:48:50 +0100

    Applied sortable to different components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { ReactSortable } from "react-sortablejs";
 import Name from "./components/Name/Name";
 import Skills from "./components/Skills/Skills";
 import ShowPreview from "./components/ShowPreview/ShowPreview";
@@ -6,17 +8,46 @@ import Formation from "./components/Formation/Formation";
 import Certificate from "./components/Certificate/Certificate";
 import Languages from "./components/Languages/Languages";
 
+interface ItemType {
+  id: number;
+  name: string;
+}
+
 function App() {
+  const [state, setState] = useState<ItemType[]>([
+    { id: 0, name: "Name" },
+    { id: 1, name: "Skills" },
+    { id: 2, name: "Experience" },
+    { id: 3, name: "Formation" },
+    { id: 4, name: "Certificate" },
+    { id: 5, name: "Languages" },
+  ]);
+
+  const componentMapping = [
+    Name,
+    Skills,
+    Experience,
+    Formation,
+    Certificate,
+    Languages,
+  ];
+
   return (
     <>
       <ShowPreview />
-      <Name />
-      <Skills />
-      <Experience />
-      <Formation />
-      <Certificate />
-      <Languages />
+      <ReactSortable
+        list={state}
+        setList={setState}
+        animation={200}
+        ghostClass="blue-background-class"
+      >
+        {state.map((item) => {
+          const Component = componentMapping[item.id];
+          return <Component key={item.id} />;
+        })}
+      </ReactSortable>
     </>
   );
 }
+
 export default App;
