8a4cd8bfa0a41074fbb342a00f733543a64d6e89
Mathiew82 <amateo1982@yahoo.es>
2024-03-01 22:36:01 +0100

    Added functionality to remove or add the skills module

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Skills.css";
 
 function Skills() {
+  const [activate, setActivate] = useState<boolean>(true);
   const [newSkill, setNewSkill] = useState<string>("");
   const [skills, setSkills] = useState<string[]>([
     "Comunicativo",
@@ -36,50 +37,73 @@ function Skills() {
     setNewSkill(value);
   };
 
+  const onRemoveSkillsModule = (): void => {
+    setActivate(false);
+  };
+
+  const addSkillsModule = (): void => {
+    setActivate(true);
+  };
+
   return (
     <>
-      <div className="editable">
-        <h2>Habilidades</h2>
-        <ul className="skills">
-          {skills.map((skill) => (
-            <li key={skill}>
-              {skill}
-              <svg
-                onClick={() => onRemoveSkill(skill)}
-                xmlns="http://www.w3.org/2000/svg"
-                className="remove-tag-button"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                fill="none"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path
-                  d="M20 6a1 1 0 0 1 .117 1.993l-.117 .007h-.081l-.919 11a3 3 0 0 1 -2.824 2.995l-.176 .005h-8c-1.598 0 -2.904 -1.249 -2.992 -2.75l-.005 -.167l-.923 -11.083h-.08a1 1 0 0 1 -.117 -1.993l.117 -.007h16zm-9.489 5.14a1 1 0 0 0 -1.218 1.567l1.292 1.293l-1.292 1.293l-.083 .094a1 1 0 0 0 1.497 1.32l1.293 -1.292l1.293 1.292l.094 .083a1 1 0 0 0 1.32 -1.497l-1.292 -1.293l1.292 -1.293l.083 -.094a1 1 0 0 0 -1.497 -1.32l-1.293 1.292l-1.293 -1.292l-.094 -.083z"
-                  fill="currentColor"
+      {activate ? (
+        <>
+          <div className="editable">
+            <h2>Habilidades</h2>
+            <ul className="skills">
+              {skills.map((skill) => (
+                <li key={skill}>
+                  {skill}
+                  <svg
+                    onClick={() => onRemoveSkill(skill)}
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="remove-tag-button"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    fill="none"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path
+                      d="M20 6a1 1 0 0 1 .117 1.993l-.117 .007h-.081l-.919 11a3 3 0 0 1 -2.824 2.995l-.176 .005h-8c-1.598 0 -2.904 -1.249 -2.992 -2.75l-.005 -.167l-.923 -11.083h-.08a1 1 0 0 1 -.117 -1.993l.117 -.007h16zm-9.489 5.14a1 1 0 0 0 -1.218 1.567l1.292 1.293l-1.292 1.293l-.083 .094a1 1 0 0 0 1.497 1.32l1.293 -1.292l1.293 1.292l.094 .083a1 1 0 0 0 1.32 -1.497l-1.292 -1.293l1.292 -1.293l.083 -.094a1 1 0 0 0 -1.497 -1.32l-1.293 1.292l-1.293 -1.292l-.094 -.083z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M14 2a2 2 0 0 1 2 2a1 1 0 0 1 -1.993 .117l-.007 -.117h-4l-.007 .117a1 1 0 0 1 -1.993 -.117a2 2 0 0 1 1.85 -1.995l.15 -.005h4z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </li>
+              ))}
+              <li className="input">
+                <input
+                  className="input-new-skill"
+                  onChange={onEditNewSkill}
+                  onKeyUp={onAddSkillWithEnter}
+                  placeholder="Ejemplo: Trabajo en equipo"
+                  value={newSkill}
                 />
-                <path
-                  d="M14 2a2 2 0 0 1 2 2a1 1 0 0 1 -1.993 .117l-.007 -.117h-4l-.007 .117a1 1 0 0 1 -1.993 -.117a2 2 0 0 1 1.85 -1.995l.15 -.005h4z"
-                  fill="currentColor"
-                />
-              </svg>
-            </li>
-          ))}
-          <li className="input">
-            <input
-              className="input-new-skill"
-              onChange={onEditNewSkill}
-              onKeyUp={onAddSkillWithEnter}
-              placeholder="Ejemplo: Trabajo en equipo"
-              value={newSkill}
+              </li>
+              <li onClick={onAddSkill} className="add">
+                añadir
+              </li>
+            </ul>
+            <button
+              onClick={onRemoveSkillsModule}
+              className="remove-module-button"
+              aria-label="Eliminar módulo"
             />
-          </li>
-          <li onClick={onAddSkill} className="add">
-            añadir
-          </li>
-        </ul>
-      </div>
+          </div>
+        </>
+      ) : (
+        <div className="wrapper-add-certificates-module">
+          <button onClick={addSkillsModule} className="add-certificates-module">
+            Agregar habilidades
+          </button>
+        </div>
+      )}
     </>
   );
 }
