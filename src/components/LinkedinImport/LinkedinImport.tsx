5f04a146746c26c568067113d6ea389a2d16b9e8
Mathiew82 <amateo1982@yahoo.es>
2026-08-01 22:36:00 +0200

    chore: change icons

--- src/components/LinkedinImport/LinkedinImport.tsx
+++ src/components/LinkedinImport/LinkedinImport.tsx
@@ -16,7 +16,9 @@ function LinkedinImport() {
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-        <path d="M5 13v-8a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2h-5.5m-9.5 -2h7m-3 -3l3 3l-3 3" />
+        <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2" />
+        <path d="M9 15h6" />
+        <path d="M12.5 17.5l2.5 -2.5l-2.5 -2.5" />
       </svg>
       Importar desde LinkedIn
     </button>
