c0c8e24aa7fadb5ab95f5e677c2b49540e11c641
Mathiew82 <amateo1982@yahoo.es>
2026-08-01 15:10:08 +0200

    style: improve styles

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -44,22 +44,70 @@ function Settings() {
   return (
     <>
       <div className="settings font-size">
-        <div>Tamaño letra: </div>
-        <button onClick={onDecreaseFontSize} title="Letra más pequeña">
-          -
-        </button>
-        <button onClick={onIncreaseFontSize} title="Letra más grande">
-          +
-        </button>
+        <div className="settings-label">Tamaño letra: </div>
+        <div className="settings-buttons">
+          <button onClick={onDecreaseFontSize} title="Letra más pequeña">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 12l14 0" />
+            </svg>
+          </button>
+          <button onClick={onIncreaseFontSize} title="Letra más grande">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 4a1 1 0 0 1 1 1v6h6a1 1 0 0 1 0 2h-6v6a1 1 0 0 1 -2 0v-6h-6a1 1 0 0 1 0 -2h6v-6a1 1 0 0 1 1 -1" />
+            </svg>
+          </button>
+        </div>
       </div>
       <div className="settings spacing">
-        <div>Espacios: </div>
-        <button onClick={onDecreaseSpacing} title="Menos espacio">
-          -
-        </button>
-        <button onClick={onIncreaseSpacing} title="Más espacio">
-          +
-        </button>
+        <div className="settings-label">Espacios: </div>
+        <div className="settings-buttons">
+          <button onClick={onDecreaseSpacing} title="Menos espacio">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 12l14 0" />
+            </svg>
+          </button>
+          <button onClick={onIncreaseSpacing} title="Más espacio">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 4a1 1 0 0 1 1 1v6h6a1 1 0 0 1 0 2h-6v6a1 1 0 0 1 -2 0v-6h-6a1 1 0 0 1 0 -2h6v-6a1 1 0 0 1 1 -1" />
+            </svg>
+          </button>
+        </div>
       </div>
     </>
   );
